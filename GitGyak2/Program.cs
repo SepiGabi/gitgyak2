ad03b5180594458e217f4ce66754cc337af14671
Gabor <sepigabi@gmail.com>
2018-11-12 10:12:51 +0100

    First commit

---
+++ GitGyak2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitGyak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
